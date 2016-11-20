ed7c19f7b53ae9c5f6b958e6212aab47f22c5e8f
Eli Segal <esegal@Administrators-MacBook-Pro-2.local>
2016-11-20 17:02:25 +0200

    Added skeleton

---
+++ src/main/java/org/rockem/tdd/bank/Account.java
@@ -0,0 +1,17 @@
+package org.rockem.tdd.bank;
+
+public class Account {
+
+    public void deposit(int amount) {
+        // IMPLEMENT ME
+    }
+
+    public void redraw(int amount) {
+        // IMPLEMENT ME
+    }
+
+    public void printStatement() {
+        // IMPLEMENT ME
+    }
+
+}
---
+++ src/test/java/test/org/rockem/tdd/bank/AccountTest.java
@@ -0,0 +1,15 @@
+package test.org.rockem.tdd.bank;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class AccountTest {
+
+    @Test
+    public void doNothing() throws Exception {
+        assertThat(true, is(true));
+
+    }
+}
